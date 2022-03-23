0bed33c426983cc660ddc89a9a305309d29e6346
Nullean <mr000lean@gmail.com>
2022-03-23 08:46:42 +0400

    added new scene

---
+++ Assets/Scripts/Commands/ScriptableObjectsTemplates/Command.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Command : ScriptableObject
+{
+    [SerializeField]
+    public readonly string CommandName;
+
+
+}
